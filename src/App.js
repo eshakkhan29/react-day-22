9a2f7e8f4286c491fbd1ef522b8722f2d546981e
eshakkhan29 <eshakkhan29@gmail.com>
2022-04-07 21:42:33 +0600

    added forget password

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ function App() {
   const [error, setError] = useState("");
   const [registered, setRegistered] = useState(false);
   const [sendmail, setSendmail] = useState(false);
+  const [user, setUser] = useState({});
+  console.log(user);
 
   //  name
   const handelName = (e) => {
@@ -60,8 +62,7 @@ function App() {
     if (!registered) {
       createUserWithEmailAndPassword(auth, email, password)
         .then((result) => {
-          console.log(result.user);
-          verifyEmail()
+          verifyEmail();
         })
         .catch((error) => {
           setError(error.message);
@@ -69,7 +70,7 @@ function App() {
     } else {
       signInWithEmailAndPassword(auth, email, password)
         .then((result) => {
-          console.log(result.user);
+          setUser(result.user);
         })
         .catch((error) => {
           setError(error.message);
@@ -77,81 +78,97 @@ function App() {
     }
     e.preventDefault();
   };
+
+  //  password reset
   const handelResetPassword = () => {
     sendPasswordResetEmail(auth, email).then(() => {
-      console.log("email send");
       setSendmail(true);
     });
   };
-  const verifyEmail = () => {
-    sendEmailVerification(auth.currentUser).then(() => {
-      setSendmail(true);
-    }).then(error => {
-      setError(error.message)
-    })
-  }
 
+  //  email verification email send
+  const verifyEmail = () => {
+    sendEmailVerification(auth.currentUser)
+      .then(() => {
+        setSendmail(true);
+      })
+      .then((error) => {
+        setError(error.message);
+      });
+  };
 
   return (
-    <div className="container mt-5 d-flex justify-content-center">
-      <div className="p-4 rounded-3">
-        {sendmail && (
-          <p className="text-success">please cheek your email inbox</p>
-        )}
-        <h2 className="text-primary">
-          {!registered ? "Register Account" : "Login Account"}
-        </h2>
-        <Form noValidate validated={validated} onSubmit={handelSubmit}>
-          {!registered && (
-            <Form.Group className="mb-3">
-              <Form.Label>Name</Form.Label>
-              <Form.Control
-                onBlur={handelName}
-                type="text"
-                placeholder="Enter Name"
-                required
-              />
-            </Form.Group>
-          )}
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              onBlur={handelEmail}
-              type="email"
-              placeholder="Enter email"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please provide a valid email.
-            </Form.Control.Feedback>
-          </Form.Group>
+    <div className="container mt-5">
+      <div>
+        <div className="row row-cols-lg-2 row-cols-1">
+          <div className="col p-4 rounded-3">
+            {sendmail && (
+              <p className="text-success fs-3">
+                please cheek your email inbox/spam
+              </p>
+            )}
+            <h2 className="text-primary">
+              {!registered ? "Register Account" : "Login Account"}
+            </h2>
+            <Form noValidate validated={validated} onSubmit={handelSubmit}>
+              {!registered && (
+                <Form.Group className="mb-3">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                    onBlur={handelName}
+                    type="text"
+                    placeholder="Enter Name"
+                    required
+                  />
+                </Form.Group>
+              )}
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  onBlur={handelEmail}
+                  type="email"
+                  placeholder="Enter email"
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid email.
+                </Form.Control.Feedback>
+              </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              onBlur={handelPassword}
-              type="password"
-              placeholder="Password"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please provide a valid password.
-            </Form.Control.Feedback>
-            <p className="text-danger">{error}</p>
-          </Form.Group>
-          { registered && <Button onClick={handelResetPassword} variant="link">
-            {" "}
-            Forget Password?
-          </Button>}
-          <div className="d-flex align-items-center">
-            <Button variant="primary" type="submit">
-              {!registered ? "Register" : "Login"}
-            </Button>
-            <Button onClick={handelRegister} variant="link">
-              {!registered ? 'Already have account?' : 'Registration'}
-            </Button>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  onBlur={handelPassword}
+                  type="password"
+                  placeholder="Password"
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid password.
+                </Form.Control.Feedback>
+                <p className="text-danger">{error}</p>
+              </Form.Group>
+              {registered && (
+                <Button onClick={handelResetPassword} variant="link">
+                  {" "}
+                  Forget Password?
+                </Button>
+              )}
+              <div className="d-flex align-items-center">
+                <Button variant="primary" type="submit">
+                  {!registered ? "Register" : "Login"}
+                </Button>
+                <Button onClick={handelRegister} variant="link">
+                  {!registered ? "Already have account?" : "Registration"}
+                </Button>
+              </div>
+            </Form>
           </div>
-        </Form>
+          {registered && <div className="col p-4">
+                <h2>{user.email}</h2>
+                <p>Email verification status: <span className=" text-success fw-bold">{user.emailVerified ? "Verified" : 'Not Verified'}</span></p>
+          </div>}
+        </div>
       </div>
     </div>
   );
