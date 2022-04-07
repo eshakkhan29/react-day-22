c6ee1d1253fa14e1bd7ebaac174b03e5de19bc47
eshakkhan29 <eshakkhan29@gmail.com>
2022-04-07 16:39:56 +0600

    condition added

--- src/App.js
+++ src/App.js
@@ -1,58 +1,118 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  sendEmailVerification,
+} from "firebase/auth";
 import app from "./firebase.init";
 import { useState } from "react";
 import { Button } from "react-bootstrap";
 const auth = getAuth(app);
 function App() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState("");
+  const [registered, setRegistered] = useState(false);
+  const [sendmail, setSendmail] = useState(false);
 
+  //  name
+  const handelName = (e) => {
+    setName(e.target.value);
+  };
+  //  email
   const handelEmail = (e) => {
     setEmail(e.target.value);
   };
+  //  password
   const handelPassword = (e) => {
     setPassword(e.target.value);
   };
-
-  const [registered, setRegistered] = useState(false);
+  //  register
   const handelRegister = () => {
-    setRegistered(!registered)
+    setRegistered(!registered);
   };
-
-  const [validated, setValidated] = useState(false);
-  const [error, setError] = useState("");
-
+  // Submit
   const handelSubmit = (e) => {
     e.preventDefault();
+
+    //  error handel
     const form = e.currentTarget;
     if (form.checkValidity() === false) {
       e.stopPropagation();
       return;
     }
+    //  password validation
     if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
       setError("At least one special character");
       return;
+    } else {
+      setError("");
     }
-    setError("");
+
     setValidated(true);
 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((result) => {
-        console.log(result.user);
-      })
-      .catch((error) => {
-        setError(error.message);
-      });
+    if (!registered) {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((result) => {
+          console.log(result.user);
+          verifyEmail()
+        })
+        .catch((error) => {
+          setError(error.message);
+        });
+    } else {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((result) => {
+          console.log(result.user);
+        })
+        .catch((error) => {
+          setError(error.message);
+        });
+    }
     e.preventDefault();
   };
+  const handelResetPassword = () => {
+    sendPasswordResetEmail(auth, email).then(() => {
+      console.log("email send");
+      setSendmail(true);
+    });
+  };
+  const verifyEmail = () => {
+    sendEmailVerification(auth.currentUser).then(() => {
+      setSendmail(true);
+    }).then(error => {
+      setError(error.message)
+    })
+  }
+
+
   return (
     <div className="container mt-5 d-flex justify-content-center">
-      <div className="w-25 p-4 rounded-3">
-        <h2 className=" text-primary">Register Account!</h2>
+      <div className="p-4 rounded-3">
+        {sendmail && (
+          <p className="text-success">please cheek your email inbox</p>
+        )}
+        <h2 className="text-primary">
+          {!registered ? "Register Account" : "Login Account"}
+        </h2>
         <Form noValidate validated={validated} onSubmit={handelSubmit}>
+          {!registered && (
+            <Form.Group className="mb-3">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                onBlur={handelName}
+                type="text"
+                placeholder="Enter Name"
+                required
+              />
+            </Form.Group>
+          )}
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control
@@ -79,11 +139,17 @@ function App() {
             </Form.Control.Feedback>
             <p className="text-danger">{error}</p>
           </Form.Group>
+          { registered && <Button onClick={handelResetPassword} variant="link">
+            {" "}
+            Forget Password?
+          </Button>}
           <div className="d-flex align-items-center">
             <Button variant="primary" type="submit">
-              Register
+              {!registered ? "Register" : "Login"}
             </Button>
-            <span onClick={handelRegister} style={{cursor:"pointer"}}  className="ms-3 text-secondary">Already have account?</span>
+            <Button onClick={handelRegister} variant="link">
+              {!registered ? 'Already have account?' : 'Registration'}
+            </Button>
           </div>
         </Form>
       </div>
